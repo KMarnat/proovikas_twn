67ca64a63c1fce88ae929f165d8608dbebe62ee5
Kaspar Marnat <kasparmarnat@gmail.com>
2024-02-14 16:41:25 +0200

    upgraded pagination to now show page numbers

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,9 +7,36 @@ interface PaginationProps {
   data: {
     list: Person[];
   };
+  itemsPerPage: number;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, setCurrentPage, data }) => {
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  setCurrentPage,
+  data,
+  itemsPerPage,
+}) => {
+  const totalPages = Math.ceil(data?.list.length / itemsPerPage);
+
+  const getPageRange = () => {
+    // Pages that are visible in the pagination, for example if current page is "7", pagination shows "5 6 7 8 9"
+    const visiblePages = 5;
+
+    // Calculates starting page and end page
+    let startPage = Math.max(1, currentPage - Math.floor(visiblePages / 2));
+    const endPage = Math.min(totalPages, startPage + visiblePages - 1);
+
+    // Start page is adjusted if the range does not fit the numbers (on page 19, it showed only 7 18 19 20). This if statement checks that and adjusts
+    if (endPage - startPage + 1 < visiblePages) {
+      startPage = Math.max(1, startPage - (visiblePages - (endPage - startPage + 1)));
+    }
+
+    // Array of the range numbers that are looped to render the pagination
+    return Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+  };
+
+  const pageNumbers = getPageRange();
+
   return (
     <div className="pagination">
       <Button
@@ -18,12 +45,23 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, setCurrentPage, da
         type="pagination-back"
         onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
       />
-      <h2>{`${currentPage} / ${data?.list.length / 10}`}</h2>
+      {pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          className={`pagination__number ${
+            pageNumber === currentPage ? "pagination__number--active" : ""
+          }`}
+          onClick={() => setCurrentPage(pageNumber)}
+        >
+          {pageNumber}
+        </button>
+      ))}
+
       <Button
         navigateBack={false}
         label="&rarr;"
         type="pagination-forward"
-        onClick={() => setCurrentPage((prev) => Math.min(prev + 1, data?.list.length / 10))}
+        onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
       />
     </div>
   );
